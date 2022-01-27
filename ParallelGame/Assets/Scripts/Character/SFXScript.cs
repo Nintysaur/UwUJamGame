4ec0df846ff0de540db620012dcd90ca98b2d666
Nintysaur <Nintysaur@gmail.com>
2022-01-27 21:53:14 +0000

    ColourUpdates :)
    Colours ahoy!! And audio, too.

---
+++ ParallelGame/Assets/Scripts/Character/SFXScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXScript : MonoBehaviour
+{
+
+    public AudioSource Jump;
+    public AudioSource Swap; 
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Swap.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump.Play();
+        }
+
+    }
+   
+ }
+
